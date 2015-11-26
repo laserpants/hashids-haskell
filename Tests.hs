9001bbdc45075d78fc75c8568e80f6d332476252
Johannes Hilden <hildenjohannes@gmail.com>
2015-11-26 16:02:10 +0300

    run tests

---
+++ Tests.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Applicative   ( (<$>) )
+import Control.Monad         ( forM_, unless )
+import Data.ByteString       ( ByteString )
+import System.Exit           ( exitSuccess, exitFailure )
+import Web.Hashids       
+
+import Data.ByteString.Char8 ( pack, unpack )
+
+pair :: [a] -> [(a, a)]
+pair []  = []
+pair [x] = []
+pair (x:y:xs) = (x, y):pair xs
+
+hashids = hashidsMinimum "this is a salt" 10
+
+enc :: String -> ByteString
+enc = encodeList hashids . read 
+
+match :: (String, String) -> Bool
+match (numbers, hash) = enc numbers == pack hash
+
+exit :: Bool -> IO ()
+exit True = exitSuccess
+exit _    = exitFailure
+
+main = do
+    file <- readFile "tests/testdata1.txt"
+    forM_ (pair $ lines file) $ \(n,h) -> 
+        unless (enc n == pack h) $ do
+            print (enc n, pack h)
+            exit False
+    exit True
+
